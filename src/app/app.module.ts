4f9c705f2f42978f26cf8cc98931e73b78157248
João Afonso Silva <joaoafonsodss@gmail.com>
2021-12-01 10:34:34 +0000

    Create project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { HomeModule } from './modules/home/home.module';
+import { PartyModule } from './modules/party/party.module';
+import { GraveyardModule } from './graveyard/graveyard.module';
+import { BattleModule } from './modules/battle/battle.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
+    AuthenticationModule,
+    HomeModule,
+    PartyModule,
+    GraveyardModule,
+    BattleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

4f14cedff08bfd499c0ffe48d59c5580d89e8b7a
João Afonso Silva <joaoafonsodss@gmail.com>
2021-12-03 02:03:56 +0000

    add header and images

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,3 +1,8 @@
+import { UserSharingService } from './../../services/user-sharing.service';
+import { UserService } from './../../services/user.service';
+import { AuthService } from './../../../core/services/auth.service';
+import { Router } from '@angular/router';
+import { UserDetails } from './../../models/user.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn!: boolean;
+  level!: number;
+  user: UserDetails = {
+    username: '',
+    email: '',
+    profilePictureUrl: '',
+    experience: 0,
+    gold: 0,
+    partyLevel: 0
+  };
 
-  constructor() { }
+
+  constructor(
+    private route: Router,
+    private authService: AuthService,
+    private userSharing: UserSharingService
+  ) { }
+
 
   ngOnInit(): void {
+    this.authService.isUserLoggedIn.subscribe(
+      value => {
+        this.isLoggedIn = value;
+        if (this.isLoggedIn === true) this.getUserSharedDetails();
+      }
+    );
+  }
+
+  goToProfile(): void {
+    this.route.navigate(['/profile']);
+  }
+
+  goToLogin(): void {
+    this.route.navigate(['/login']);
+  }
+
+  private getUserSharedDetails(): void {
+    this.userSharing.getUserDetails();
+    this.userSharing.sharedUser.subscribe(
+      value => {
+        this.user = value
+        this.level = (this.user.experience / 10) / (Math.log10(this.user.experience + 10)) + 1;
+      }
+    );
   }
 
 }

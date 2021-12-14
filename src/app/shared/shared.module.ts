6e6e9ff3b1d297e200592cb827952754ce16c487
João Afonso Silva <joaoafonsodss@gmail.com>
2021-12-14 16:25:20 +0000

    help page

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BackButtonComponent } from './components/back-button/back-button.component';
 import { CharacterGridItemComponent } from './components/character-grid-item/character-grid-item.component';
-import { SideDrawerComponent } from './components/side-drawer/side-drawer.component';
 import { CharacterDetailsComponent } from './components/character-details/character-details.component';
-import { HelperComponent } from './components/helper/helper.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -22,9 +20,7 @@ import { HelpPageComponent } from './components/help-page/help-page.component';
     FooterComponent,
     BackButtonComponent,
     CharacterGridItemComponent,
-    SideDrawerComponent,
     CharacterDetailsComponent,
-    HelperComponent,
     HelpPageComponent
   ],
   imports: [
@@ -41,9 +37,7 @@ import { HelpPageComponent } from './components/help-page/help-page.component';
     FooterComponent,
     BackButtonComponent,
     CharacterGridItemComponent,
-    SideDrawerComponent,
     CharacterDetailsComponent,
-    HelperComponent,
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,

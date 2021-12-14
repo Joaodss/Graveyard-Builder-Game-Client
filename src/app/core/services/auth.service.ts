548ff9aa47f87a378f1ca7d6a16900bb78cffde3
João Afonso Silva <joaoafonsodss@gmail.com>
2021-12-14 20:38:56 +0000

    change requests ip

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -8,7 +8,7 @@ import * as moment from 'moment';
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly baseUrl = "http://localhost:8000"
+  private readonly baseUrl = "http://1:8000"
   public isUserLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(this.isLoggedIn());
 
   constructor(
